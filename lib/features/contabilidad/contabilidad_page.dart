15020fd0cb5d6d6ec491434ddf8836c1e1cf8831
emili <emilianoalbamonte@gmail.com>
2024-05-24 18:32:40 +0200

    feat: Update ContabilidadPage and GestEmpleadosPage UI
    - Updated the UI of ContabilidadPage to display the text "Contabilidad".
    - Updated the UI of GestEmpleadosPage to display the text "Gestión de Empleados".

--- lib/features/contabilidad/contabilidad_page.dart
+++ lib/features/contabilidad/contabilidad_page.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 
 class ContabilidadPage extends StatelessWidget {
   const ContabilidadPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Contabilidad Page')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => Modular.to.navigate('/second'),
-          child: const Text('Navigate to Second Page'),
-        ),
-      ),
+    return  const Center(
+          child:  Text('Contabilidad'),
     );
   }
 }
--- lib/features/gestion_empleados/gest_empleados_page.dart
+++ lib/features/gestion_empleados/gest_empleados_page.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 
 class SecondPage extends StatelessWidget {
   const SecondPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Gestión Empleados')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => Modular.to.navigate('/'),
-          child: const Text('Back to Home'),
-        ),
-      ),
+    return const Center(
+      child: Text('Gestión de Empleados'),
     );
   }
 }
